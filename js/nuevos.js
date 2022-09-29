8244ab13131036794d69902e1746305c799604cb
Matias Sivadon <sivadon.m@gmail.com>
2022-09-29 17:19:57 -0300

    descomentado funcion recuperarCarrito en nuevos

--- js/nuevos.js
+++ js/nuevos.js
@@ -34,9 +34,9 @@ const agregarAlCarrito = () => {
 }
 
 function recuperarCarrito() {
-    // if (localStorage.getItem("carrito")) {
-    //     carrito = JSON.parse(localStorage.getItem("carrito"))
-    // }
+    if (localStorage.getItem("carrito")) {
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+    }
     localStorage.getItem("carrito") && JSON.parse(localStorage.getItem("carrito")) 
 }
 recuperarCarrito()
